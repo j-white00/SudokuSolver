e549cd1507f8f5b2188f80434bb9d02f4c95320a
j-white00 <61454249+j-white00@users.noreply.github.com>
2020-10-21 01:44:27 +0100

    Window Running

--- pygameTest.py
+++ pygameTest.py
@@ -1,6 +1,14 @@
-import pygame, time
+import pygame
 pygame.init()
-while True: 
-    win = pygame.display.set_mode((500,500))
 
+WHITE = (255,255,255)
+
+running = True
+while running:
+    win = pygame.display.set_mode((500,500))
+    win.fill(WHITE)
+    for event in pygame.event.get():
+        pygame.display.update()
+        if event.type == pygame.QUIT:
+            running = False
 pygame.quit()
