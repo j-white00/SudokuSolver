03848430dc69cd523497d2c532b8a11509c6319f
j-white00 <61454249+j-white00@users.noreply.github.com>
2020-10-26 22:51:36 +0000

    Pygame Window Opening and Closing

--- pygameTest.py
+++ pygameTest.py
@@ -1,14 +1,25 @@
-import pygame
-pygame.init()
+import pygame, sys
+from pygame.locals import *
 
-WHITE = (255,255,255)
+col_white = (255, 255, 255)
+col_black = (0, 0, 0)
 
-running = True
-while running:
-    win = pygame.display.set_mode((500,500))
-    win.fill(WHITE)
-    for event in pygame.event.get():
+def main():
+    global win_clock, win_display
+    pygame.init()
+    pygame.display.set_caption("Sudoku Solver")
+    win_clock = pygame.time.Clock()
+    win_display = pygame.display.set_mode((500, 500))
+
+    running = True
+    while running:
+        win_display.fill(col_white)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
         pygame.display.update()
-        if event.type == pygame.QUIT:
-            running = False
-pygame.quit()
+        win_clock.tick(5)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
